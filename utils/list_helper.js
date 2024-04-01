bc81baf97e608e1fa0c71ef56d927d032d560016
Andres Morales <moralesandres@outlook.com>
2024-04-01 16:29:11 -0500

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,14 @@
 const dummy = (blogs) => {
     return 1
   }
+
+  const totalLikes = (blogs) => {
+    const suma = (sum, blog) => {
+      return sum + blog.likes
+    }
+    return blogs.reduce(suma, 0)
+  }
   
   module.exports = {
-    dummy
+    dummy, totalLikes
   }
