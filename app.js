42019936b2f18f5dc4616837ae36d4eaa906a389
Andres Morales <moralesandres@outlook.com>
2024-04-24 13:33:31 -0500

    4.15

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 
@@ -17,5 +18,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
