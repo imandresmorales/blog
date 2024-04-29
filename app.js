d75a25b2002b4235afe9ae5137300ee596656262
Andres Morales <moralesandres@outlook.com>
2024-04-29 13:11:44 -0500

    4.18

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const usersRouter = require('./controllers/users')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 mongoose.connect(config.MONGODB_URI)
@@ -23,6 +24,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
