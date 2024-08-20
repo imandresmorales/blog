4181c140dabfe0ad21764664629a41cb14e60c05
Andres Morales <moralesandres@outlook.com>
2024-08-20 18:17:43 -0500

    update for part 7

--- app.js
+++ app.js
@@ -24,9 +24,15 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
 
+app.use('/', blogsRouter)//la agregue para que se pueda acceder a la pagina principal sin /api/blogs
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+if(process.env.NODE_ENV === 'test'){
+    const testingRouter = require('./controllers/testing')
+    app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,78 +1,89 @@
-const blogsRouter = require('express').Router()
-const { request } = require('../app')
-const Blog = require('../models/blog')
-const User = require('../models/user')
-const helper = require('../tests/test_helper')
-const jwt = require('jsonwebtoken')
-const middleware = require('../utils/middleware')
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
+const User = require("../models/user");
+const helper = require("../tests/test_helper");
+const jwt = require("jsonwebtoken");
+const middleware = require("../utils/middleware");
 
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
 
-blogsRouter.get('/', async (request, response) => {
-  const blog = await Blog.find({}).populate('user', {username: 1, name: 1})
-  response.json(blog)
-})
+blogsRouter.get("/", async (request, response) => {
+  const blog = await Blog.find({}).populate("user", { username: 1, name: 1 });
+  response.json(blog);
+});
 
-  blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
-    const body = request.body
-    // console.log(body)
-    if(!body.title || !body.url){
-      return response.status(400).end()
-    }
-    const user = request.user
-    
-    const blog = new Blog({
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes || 0,
-        user: user.id
-    })
+blogsRouter.get("/:id", async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
+  response.json(blog);
+});
 
-    const saveBlog = await blog.save()
-    user.blogs =  user.blogs.concat(saveBlog._id)
-    await user.save()
-    response.status(201).json(saveBlog)
-  })
+blogsRouter.post("/", middleware.userExtractor, async (request, response) => {
+  const body = request.body;
+  // console.log(body)
+  if (!body.title || !body.url) {
+    return response.status(400).end();
+  }
+  const user = request.user;
 
-  blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
-    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
+    user: user.id,
+    comments: [],
+  });
+
+  const saveBlog = await blog.save();
+  user.blogs = user.blogs.concat(saveBlog._id);
+  await user.save();
+  response.status(201).json(saveBlog);
+});
+
+blogsRouter.delete(
+  "/:id",
+  middleware.userExtractor,
+  async (request, response) => {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET);
     // console.log(decodedToken)
     if (!decodedToken.id) {
-      return response.status(401).json({ error: 'token invalid' })
+      return response.status(401).json({ error: "token invalid" });
     }
-    const user = request.user
-    const blog = await Blog.findById(request.params.id)
+    const user = request.user;
+    const blog = await Blog.findById(request.params.id);
     if (!blog) {
-      return response.status(401).json({ error: 'blog invalid' })
+      return response.status(401).json({ error: "blog invalid" });
     }
-    
-    if ( blog.user.toString() === user.id.toString() ) {
-      await Blog.findByIdAndDelete(request.params.id)
-      response.status(204).end()
-    }
-    else{
-      response.status(401).json({ error: 'invalid operation' })
-    }
-  })
 
-  blogsRouter.put('/:id', async (request, response) => {
-    const body = request.body
-    const blog = {
-      title: body.title,
-      author: body.author,
-      url: body.url,
-      likes: body.likes
+    if (blog.user.toString() === user.id.toString()) {
+      await Blog.findByIdAndDelete(request.params.id);
+      response.status(204).end();
+    } else {
+      response.status(401).json({ error: "invalid operation" });
+    }
   }
- 
-    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
-    response.json(updatedBlog)
-  })
+);
 
-    module.exports = blogsRouter
+blogsRouter.put("/:id", async (request, response) => {
+  const body = request.body;
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    comments: body.comments,
+  };
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  });
+  response.json(updatedBlog);
+});
+
+module.exports = blogsRouter;
